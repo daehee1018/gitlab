c6d22f5809d18f59b9e58b5fa9e74f3afffa4143
daehee1018 <handh011018@gmail.com>
2024-12-07 15:20:36 +0000

    testdd

--- newfeature.c
+++ newfeature.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
 void main(){
-  char pwd[1024];
-  FILE *F = fopen("pwd=log.log",'w');
-  getcwd(pwd, sizeof(pwd));
-  printf("print working dir : %s\n, pwd);
-  fputs("%s\n", f);
-  fclose(f);
+	//dd
+	////ssss
 }
